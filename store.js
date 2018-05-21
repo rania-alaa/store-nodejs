8b62d93c703fb024e69ae0be855abdcc484bac7b
rania-alaa <rania.alaaeldien.mohamed@gmail.com>
2018-05-21 09:34:44 +0200

    Revert "change storing method"
    This reverts commit 4a5c3eb536da8e2308643351de8294be707d8a2d.

--- store.js
+++ store.js
@@ -1,90 +1,77 @@
 var fs = require('fs');
 var path = require('path');
 
-var fun = process.argv.splice(2)
-switch (fun[0]) {
-   case 'add':
-     add(fun[1],fun[2]);
-     break;
-   case 'list':
-     list();
-     break;
-   case 'get':
-     get(fun[1]);
-     break;
-   case 'remove':
-     remove(fun[1]);
-     break;
-   case 'clear':
-     clear();
-     break;
- }
- function add(mykey, myvalue){
-   var dicts = [];
-   var dictAsJSON = fs.readFileSync(path.join(__dirname,"db.db")).toString()
-   if (dictAsJSON != "") {
+module.exports = {
+    add: function(mykey,myvalue){
+      var dicts = [];
+      var dictAsJSON = fs.readFileSync(path.join(__dirname,"db.db")).toString()
+      if (dictAsJSON != "") {
         dicts = JSON.parse(dictAsJSON);
-   }
-   dicts.push({
-     key: mykey ,
-     value: myvalue
-   });
-   fs.writeFileSync(path.join(__dirname,"db.db"),JSON.stringify(dicts))
-   console.log("done adding");
- }
- function list(){
-   var dictAsJSON = fs.readFileSync(path.join(__dirname,"db.db")).toString()
-   if (dictAsJSON == "") {
-         return "No Keys "
-   }
-   else {
-     var dicts = JSON.parse(dictAsJSON);
-     console.log("the avalible key and values:");
-     console.log(dicts);
-   }
- }
- function get(mykey){
-   var selecteddict = "no such dictionary" ;
-   var dictAsJSON = fs.readFileSync(path.join(__dirname,"db.db")).toString()
-   if (dictAsJSON == "") {
-     console.log("No Keys avalible to get it, please add a key first ");
-   }
-   else {
-     var dicts = JSON.parse(dictAsJSON);
-     dicts.forEach(function(dict) {
-       if (dict.key == mykey) {
-         selecteddict = dict
-       }
-     });
-   }
-    console.log(selecteddict);
- }
- function remove(mykey){
-   var selecteddict ;
-   var dictAsJSON = fs.readFileSync(path.join(__dirname,"db.db")).toString()
-   if (dictAsJSON == "") {
-     return "No Keys avalible to remove it, please add a key first "
-   }
-   else{
-     var dicts = JSON.parse(dictAsJSON);
-     dicts.forEach(function(dict) {
-       if (dict.key == mykey) {
-         selecteddict = dict
-         var index= dicts.indexOf(selecteddict)
-         dicts.splice(index,1)
-         fs.writeFileSync(path.join(__dirname,"db.db"),JSON.stringify(dicts))
-         selecteddict = selecteddict.key +" key removed" ;
-       }
-       else {
-         selecteddict = "no such dictionary" ;
-       }
-     });
-   }
-    console.log(selecteddict);
- }
- function clear(){
-   var cler = "" ;
-         fs.writeFileSync(path.join(__dirname,"db.db"),cler)
-         var dictAsJSON = fs.readFileSync(path.join(__dirname,"db.db")).toString()
-         console.log("cleared");
- }
+      }
+      dicts.push({
+        key: mykey ,
+        value: myvalue
+      });
+      fs.writeFileSync(path.join(__dirname,"db.db"),JSON.stringify(dicts))
+      return "done adding"
+    },
+    list: function(){
+      var dictAsJSON = fs.readFileSync(path.join(__dirname,"db.db")).toString()
+      if (dictAsJSON == "") {
+        return "No Keys "
+      }
+      else {
+        var dicts = JSON.parse(dictAsJSON);
+        console.log("the avalible key and values:");
+        return dicts
+      }
+    },
+    get: function(mykey){
+      var selecteddict = "no such dictionary" ;
+      var dictAsJSON = fs.readFileSync(path.join(__dirname,"db.db")).toString()
+      if (dictAsJSON == "") {
+        return "No Keys avalible to get it, please add a key first "
+      }
+      else {
+        var dicts = JSON.parse(dictAsJSON);
+        dicts.forEach(function(dict) {
+          if (dict.key == mykey) {
+            selecteddict = dict
+          }
+        });
+      }
+      return selecteddict
+    },
+    remove: function(mykey){
+      var selecteddict ;
+      var dictAsJSON = fs.readFileSync(path.join(__dirname,"db.db")).toString()
+      if (dictAsJSON == "") {
+        return "No Keys avalible to remove it, please add a key first "
+      }
+      else{
+        var dicts = JSON.parse(dictAsJSON);
+        dicts.forEach(function(dict) {
+          if (dict.key == mykey) {
+            selecteddict = dict
+            var index= dicts.indexOf(selecteddict)
+            dicts.splice(index,1)
+            fs.writeFileSync(path.join(__dirname,"db.db"),JSON.stringify(dicts))
+            selecteddict = selecteddict.key +" key removed" ;
+          }
+          else {
+            selecteddict = "no such dictionary" ;
+          }
+        });
+      }
+      return selecteddict
+    },
+    clear: function(){
+      var cler = "" ;
+      fs.writeFileSync(path.join(__dirname,"db.db"),cler)
+      var dictAsJSON = fs.readFileSync(path.join(__dirname,"db.db")).toString()
+      return "cleared"
+    }
+};
+require('make-runnable/custom')({
+    printOutputFrame: false
+})
